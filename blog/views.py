2f1baaab0446e3ac3b87bd5eafbdfe7a9f669e33
Mathewclementina <your-email@example.com>
2025-09-28 08:51:29 -0700

    Added search bar and no results message

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 
-# Show all posts
+# Show all posts + search
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    query = request.GET.get("q")
+    if query:
+        posts = Post.objects.filter(title__icontains=query).order_by('-created_at')
+    else:
+        posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts, 'query': query})
+
 
 # Show one post
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 # Create a post
 def post_create(request):
